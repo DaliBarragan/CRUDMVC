04360eeeb09a54b874ee473e7b099fdace072014
Alan Barragan <dalibarragan09@gmail.com>
2025-06-09 18:01:08 -0600

    Primer Commit

---
+++ CRUDMVC/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDMVC.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+                        
+        }
+        
+        //AGREGAR MODELOS AQUI
+    }
+}
