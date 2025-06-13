1bd82c172544c5587b4000b7c687ddad0ee2e79e
Alan Barragan <dalibarragan09@gmail.com>
2025-06-12 18:00:51 -0600

    Proyecto CRUDMVC completado, todas las funcionalidades de un CRUD en MVC .NET

--- CRUDMVC/Controllers/InicioController.cs
+++ CRUDMVC/Controllers/InicioController.cs
@@ -17,9 +17,104 @@ namespace CRUDMVC.Controllers
             _contexto = contexto;
         }
 
+        [HttpGet]
+
         public async Task<IActionResult> Index()
         {
             return View(await _contexto.Contacto.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                //Agregar fecha y hora actual
+                contacto.FechaCreacion = DateTime.Now;
+
+                _contexto.Contacto.Add(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+
+                _contexto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            return View(contacto);
+        }
+
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto (int? id)
+        {
+            var contacto = await _contexto.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+                return View();
+            }
+
+            //Borrado
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index)); 
+
         }
 
         public IActionResult Privacy()
